061185fc11d12336f3c6152aee7087cbae7ad594
App Academy Student <student@appacademy.io>
2013-03-26 18:04:34 -0700

    ai in progress

--- lib/game.rb
+++ lib/game.rb
@@ -7,6 +7,7 @@ class Game
   def initialize
     @board = Board.new
     @players = [:red, :blue]
+    @ai_player = [:red]
   end
 
   def input(player)
@@ -34,7 +35,7 @@ class Game
        @board.rows[x][y].change_color
      end
 
-     player = (player == @players[0]) ? @players[1] : @players[0]
+     player = (player == @players[0]) ? @ai_player : @players[0]
 
     end
   end
@@ -51,6 +52,45 @@ class Game
     end
 
     false
+  end
+  def ai_play
+    player = @players[1]
+    until end?(player)
+     @board.render
+     received_move = false
+
+     until received_move || end?(player)
+       move, color = input(player)
+       received_move = @board.valid_move?(move, color)
+     end
+     to_flip = @board.pieces_to_flip(move, color)
+     @board.place_piece(move, color)
+     to_flip.each do |x, y|
+       @board.rows[x][y].change_color
+     end
+
+     player = (player == @players[0]) ? @players[1] : @players[0]
+
+    end
+
+  end
+  def ai_player #input but for AI
+    best_play  = [nil, 0]
+    @board.rows.each do |row|
+      row.each do |spot|
+        if spot.nil?
+          if @board.valid_move?([row,spot], :red)
+
+            count = @board.piece_to_flip([row,spot], :red).count
+            if count > best_play[1]
+              best_play = [[row, spot], count]
+            end
+          end
+        end
+      end
+    end
+
+    best_play
   end
 
 
